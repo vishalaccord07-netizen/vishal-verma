1fb34af090df16ae1fe8bb43b023fec0c1ca1f7a
vishalaccord07-netizen <vishal.accord07@gmail.com>
2026-08-21 16:47:13 +0530

    Add Android app source: app/build.gradle.kts

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.outletpulse.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.outletpulse.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+}
+
